af59da5514f95af5aa928bbd560da50098437fe3
Roberto <robertosilvati@gmail.com>
2023-06-02 20:04:55 -0300

    atualização de arquivos

---
+++ forca.py
@@ -0,0 +1,16 @@
+
+def jogar():
+    print("*************************************")
+    print("Bem vindo ao jogo de Forca em Python!")
+    print("*************************************")
+
+
+
+
+
+
+
+
+
+if(__name__ == "__main__"):
+    jogar()

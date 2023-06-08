ab28104ca3ce09f6f4b797cba54bbe564353e413
Roberto <robertosilvati@gmail.com>
2023-06-08 12:31:37 -0300

    atualização jogo da forma e incluindo arquivo palavras.txt

--- forca.py
+++ forca.py
@@ -1,29 +1,55 @@
+import random
 
 def jogar():
-    print("*************************************")
-    print("Bem vindo ao jogo de Forca em Python!")
-    print("*************************************")
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
-    
-    print(letras_acertadas)
+
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0,len(palavras))
+    palavra_secreta = palavras[numero].upper()
+
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
-    #enquanto não enforcou E não acertou
-    while (not acertou and not enforcou):
+    erros = 0
+
+    print(letras_acertadas)
+
+    while(not enforcou and not acertou):
 
         chute = input("Qual letra? ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index = 0
-        for letra in palavra_secreta:
-            if (chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-            index = index + 1
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
 
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
+
+
+    if(acertou):
+        print("Você ganhou!!")
+    else:
+        print("Você perdeu!!")
+    print("Fim do jogo")
 
 if(__name__ == "__main__"):
     jogar()
